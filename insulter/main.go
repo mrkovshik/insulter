761edcdff9a1330ce44ee5ac3025eac9bcde4d73
kolya <kovshik@inbox.ru>
2022-11-24 09:42:34 +0300

    initial commit

---
+++ insulter/main.go
@@ -0,0 +1,15 @@
+// main.go
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+mux:= http.NewServeMux()
+mux.HandleFunc("/name/", Insult)
+mux.HandleFunc("/age/", deathClocker)
+
+	 log.Fatal(http.ListenAndServe(":5000", mux))
+}
